33e3961c85bc5d7036e723728f28961c28780ba3
ainuser <979111986@qq.com>
2022-08-21 20:43:13 +0800

    feat: 我的页面

--- pages/tabbar/me/me.vue
+++ pages/tabbar/me/me.vue
@@ -1,6 +1,265 @@
+<style lang="scss" scoped>
+	.content {
+		background-color: #fafafa;
+		text-align: center;
+		height: 100vh;
+	}
+	.top {
+	  height: 250rpx;
+	  position: relative;
+	  .background {
+	    background-color: #5199ff;
+	    border-bottom-left-radius: 22px;
+	    border-bottom-right-radius: 22px;
+	    position: absolute;
+	    height: 180rpx;
+	    width: 100%;
+	  }
+	}
+	.icon {
+	  color: #96a1ae;
+	  font-size: 24rpx;
+	  float: right;
+	}
+	.user-card {
+	  height: 150rpx;
+	  padding: 0 15px;
+	  .card {
+	    position: relative;
+	    bottom: 72px;
+	    height: 250rpx;
+	    background-color: #ffffff;
+	    border-radius: 5px;
+		box-shadow: 0 0 15px 1px rgba(224, 224, 224, 50%);
+	    .top {
+	      height: 30%;
+	      position: relative;
+	      .userImage {
+			background-color: white;
+	        position: relative;
+	        bottom: 40px;
+			margin: 0 auto;
+	        width: 190rpx;
+	        height: 150rpx;
+	        overflow: hidden;
+	        border-radius: 50%;
+	        border: 1px solid #eee;
+			image {
+				display: initial;
+			}
+	      }
+	    }
+	    .bottom {
+	      display: flex;
+	      height: 70%;
+	      .left {
+	        width: 100%;
+	        height: 100%;
+	        position: relative;
+	        .user-text {
+	          width: 100%;
+	          font-size: 1.4em;
+			  text-align: center;
+			  .userNickName {
+				  padding-top: 10px;
+				  display: inline-block;
+			  }
+			  image {
+				  height: 25px;
+				  width: 25px;
+				  padding-left: 5px;
+				  line-height: 200%;
+			  }
+	        }
+	        .user-phone {
+	          width: 100%;
+			  text-align: center;
+			  padding-top: 5px;
+	        }
+	      }
+	      .right {
+	        width: 20%;
+	        height: 50%;
+	      }
+	    }
+	  }
+	}
+	.list-card {
+	  background-color: #ffffff;
+	  margin: 20px 15px 0 15px;
+	  border-radius: 5px;
+	  box-shadow: 0 0 15px 1px rgba(224, 224, 224, 50%);
+	  .card {
+	    border-radius: 5px;
+	    position: relative;
+	    background-color: white;
+	    border-radius: 5px;
+	    .item {
+	      display: flex;
+	      height: 120rpx;
+	      .left {
+	        width: 15%;
+	        image {
+	          width: 40rpx;
+	          height: 40rpx;
+			  padding: 22px 2px;
+	        }
+	      }
+	      .center {
+	        width: 70%;
+	        display: flex;
+	        justify-content: start;
+	        align-items: center;
+	        font-size: 1.1em;
+	      }
+	      .right {
+	        width: 15%;
+	        padding: 25px 2px;
+	      }
+	    }
+	  }
+	}
+	.item-bottom-solid {
+	  border-bottom: 1px solid #d4d6da;
+	}
+	.func {
+		display: flex;
+		height: 280upx;
+		align-items: center;
+		background-color: #ffffff;
+		flex-wrap: wrap;
+		margin: 0 15px;
+		border-radius: 5px;
+		box-shadow: 0 0 15px 1px rgba(224, 224, 224, 50%);
+		.option {
+			width: 25%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			height: 140upx;
+			.icon {
+				image {
+					height: 50upx;
+					width: 50upx;
+				}
+			}
+			.name {
+				color: #333;
+				line-height: 30upx;
+			}
+		}
+	}
+</style>
 <template>
+	<!-- 个人信息区域 -->
 	<view class="content">
-		页面 - 5
+	  <view class="top">
+	    <view class="background"></view>
+	  </view>
+	  <view class="user-card">
+	    <view class="card">
+	      <view class="top">
+	        <view class="userImage">
+	          <image src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Flmg.jj20.com%2Fup%2Fallimg%2Ftx19%2F190400013682702.jpg&refer=http%3A%2F%2Flmg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1663558223&t=18f5f6031a8617c79e371aa78bcef759" mode="aspectFit"></image>
+			</view>
+	      </view>
+	      <view class="bottom">
+	        <view class="left">
+	          <view class="user-text">
+	            <view class="userNickName">你是我的优乐美</view>
+				<image mode="aspectFit" src="../../../static/me/boy.svg"></image>
+				<view class="option" @click="goto('/pages/tabbar/me/user')">
+					<u-icon class="icon" name="arrow-right"></u-icon>
+				</view>
+	          </view>
+	          <view class="user-phone">因为有你，所以一切变得美好</view>
+	        </view>
+	      </view>
+	    </view>
+	  </view>
+	  
+	  <!-- 功能区域 -->
+	  <view class="func">
+	      <view class="option" @click="">
+	      	<view class="icon">
+	      		<image src="../../../static/me/daishenhe.svg" mode="aspectFit"></image>
+	      	</view>
+	      	<view class="name">待审核</view>
+	      </view>
+	      <view class="option" @click="">
+	      	<view class="icon">
+	      		<image src="../../../static/me/publish.svg" mode="aspectFit"></image>
+	      	</view>
+	      	<view class="name">已发布</view>
+	      </view>
+		  <view class="option" @click="">
+		  	<view class="icon">
+		  		<image src="../../../static/me/xiajia.svg" mode="aspectFit"></image>
+		  	</view>
+		  	<view class="name">已下架</view>
+		  </view>
+		  <view class="option" @click="">
+		  	<view class="icon">
+		  		<image src="../../../static/me/save.svg" mode="aspectFit"></image>
+		  	</view>
+		  	<view class="name">收藏记录</view>
+		  </view>
+		  <view class="option" @click="">
+		  	<view class="icon">
+		  		<image src="../../../static/me/quanzi.svg" mode="aspectFit"></image>
+		  	</view>
+		  	<view class="name">圈子</view>
+		  </view>
+		  <view class="option" @click="">
+		  	<view class="icon">
+		  		<image src="../../../static/me/hudong.svg" mode="aspectFit"></image>
+		  	</view>
+		  	<view class="name">互动</view>
+		  </view>
+		  <view class="option" @click="">
+		  	<view class="icon">
+		  		<image src="../../../static/me/share.svg" mode="aspectFit"></image>
+		  	</view>
+		  	<view class="name">转发</view>
+		  </view>
+		  <view class="option" @click="">
+		  	<view class="icon">
+		  		<image src="../../../static/me/look.svg" mode="aspectFit"></image>
+		  	</view>
+		  	<view class="name">浏览</view>
+		  </view>
+	  </view>
+	  
+	  <!-- 横条区域 -->
+	  <view class="list-card">
+	    <view class="card">
+	      <view class="item item-bottom-solid">
+	        <view class="left flex-center">
+	          <image mode="aspectFit" src="../../../static/me/share.svg"></image>
+	        </view>
+	        <view class="center">
+	          <text>分享返现</text>
+	        </view>
+	        <view class="right flex-center">
+	          <u-icon class="icon" name="arrow-right"></u-icon>
+	        </view>
+	      </view>
+	    </view>
+	    <view class="card">
+	      <view class="item item-bottom-solid">
+	        <view class="left flex-center">
+			  <image mode="aspectFit" src="../../../static/me/about.svg"></image>
+	        </view>
+	        <view class="center">
+	          <text>关于我们</text>
+	        </view>
+	        <view class="right flex-center">
+	          <u-icon class="icon" name="arrow-right"></u-icon>
+	        </view>
+	      </view>
+	    </view>
+	  </view>
 	</view>
 </template>
 
@@ -15,15 +274,12 @@
 
 		},
 		methods: {
-
+			goto(uri){
+				console.log(uri)
+				uni.navigateTo({
+					url: '/pages/tabbar/me/user'
+				})
+			}
 		}
 	}
 </script>
-
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
-		margin-top: 200upx;
-	}
-</style>
---
+++ pages/tabbar/me/user.vue
@@ -0,0 +1,200 @@
+<style>
+    page{
+        width: 100%;
+        height: 100%;
+        background-color: #fafafa;
+    }
+    .cont_view{
+        width: 100%;
+        height: 100%;
+        background-color: #fafafa;
+        overflow-y: auto;
+        box-sizing: border-box;
+    }
+    .person_info_view{
+        padding: 0 40upx;
+        box-sizing: border-box;
+        background-color: #ffffff;
+		box-shadow: 0 0 15px 1px rgba(224, 224, 224, 50%);
+    }
+    .item:last-child{
+        border-bottom: none;
+    }
+    .item{
+        height: 110upx;
+        border-bottom: 1upx solid #ebebeb;
+        box-sizing: border-box;
+        justify-content: space-between;
+		display: flex;
+        align-items: center;
+    }
+    .item_text{
+        font-family: PingFang-SC-Medium;
+        font-size: 30upx;
+        color: #191f25;
+        font-weight: bold;
+        flex-shrink: 0;
+    }
+    .img_view_info{
+        align-items: center;
+        flex-shrink: 0;
+    }
+    .img_view{
+        width: 110upx;
+        height: 110upx;
+        margin: auto 0;
+        flex-shrink: 0;
+		display: flex;
+        border-radius: 100%;
+    }
+    .img_view > image{
+        width: 100%;
+        height: 100%;
+        display: flex;
+		image{
+			float: right;
+		}
+    }
+    .arrow_right{
+        width: 28upx;
+        height: 28upx;
+        flex-shrink: 0;
+        margin: auto 0;
+		color: #a3a5a8;
+		margin-left: 5px;
+    }
+    .item_val{
+        font-size: 30upx;
+        color: #a3a5a8;
+        text-align: right;
+        max-width: 100%;
+        padding-left: 40upx;
+        box-sizing: border-box;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+    }
+	.box-bg {
+		background-color: #F5F5F5;
+		padding: 5px 0;
+	}
+</style>
+<template>
+    <view class="cont_view">
+        <view class="person_info_view">
+            <!--头像-->
+            <view class="item f_r_b" @click="chooseAvatar">
+                <view class="item_text">头像</view>
+                <view class="f_r_e img_view_info">
+                    <view class="img_view">
+						<image @error="imgError" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Flmg.jj20.com%2Fup%2Fallimg%2Ftx19%2F190400013682702.jpg&refer=http%3A%2F%2Flmg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1663558223&t=18f5f6031a8617c79e371aa78bcef759" mode="aspectFit"></image>
+						<u-icon class="arrow_right" name="arrow-right"></u-icon>
+					</view>
+                </view>
+            </view>
+            <!--昵称-->
+            <view class="item f_r_b">
+                <view class="item_text">昵称</view>
+                <view class="item_val">
+					你是我的优乐美
+					<u-icon class="arrow_right" name="arrow-right"></u-icon>
+				</view>
+				
+            </view>
+            <!--性别-->
+            <view class="item f_r_b">
+                <view class="item_text">性别</view>
+                <view class="item_val">
+					女
+					<u-icon class="arrow_right" name="arrow-right"></u-icon>
+				</view>
+				
+            </view>
+            <!--地区-->
+            <view class="item f_r_b">
+                <view class="item_text">地区</view>
+                <view class="item_val">
+					北京 海淀
+					<u-icon class="arrow_right" name="arrow-right"></u-icon>
+				</view>
+				
+            </view>
+            <!--手机号-->
+            <view class="item f_r_b">
+                <view class="item_text">手机号</view>
+                <view class="item_val">
+					132****3291
+					<u-icon class="arrow_right" name="arrow-right"></u-icon>
+				</view>
+				
+            </view>
+            <!--实名认证-->
+            <view class="item f_r_b">
+                <view class="item_text">实名认证</view>
+                <view class="item_val">
+					未认证
+					<u-icon class="arrow_right" name="arrow-right"></u-icon>
+				</view>
+				
+            </view>
+			<!--修改密码-->
+			<view class="item f_r_b">
+			    <view class="item_text">修改密码</view>
+			    <view class="item_val">
+					修改密码
+					<u-icon class="arrow_right" name="arrow-right"></u-icon>
+				</view>
+			</view>
+			<!--个性签名-->
+			<view class="item f_r_b">
+			    <view class="item_text">个性签名</view>
+			    <view class="item_val">
+					因为有你，所以一切变得美好
+					<u-icon class="arrow_right" name="arrow-right"></u-icon>
+				</view>
+			</view>
+        </view>
+    </view>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        onLoad() {
+           
+        },
+        onShow() {
+            
+        },
+        onUnload() {
+            
+        },
+        methods: {
+            imgError() {
+                // 这里填充一个默认图片
+            },
+            // 更新头像
+            chooseAvatar() {
+                uni.showActionSheet({
+                    itemList: ['拍照', '从手机相册选择'],
+                    success: (res) => {
+                        this.uploadImg(res.tapIndex === 0 ? "camera" : "album")
+                    }
+                });
+            },
+            // 上传图片
+            uploadImg(sourceType = "camera") {
+                uni.chooseImage({
+                    count: 1,
+                    sizeType: ['compressed'],
+                    sourceType: [sourceType],
+                    success: function (res) {
+                        uni.showLoading({title: '上传中...', mask: Config.mask});
+                        // 上传图片操作 (.then().catch(err => {}))
+                    }
+                });
+            }
+        }
+    }
+</script>
