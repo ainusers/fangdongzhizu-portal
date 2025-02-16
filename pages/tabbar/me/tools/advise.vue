d6b48b81e4fc76d9e5b7e3c8ec8039e4496e582f
979111986@qq.com <979111986@qq.com>
2025-02-16 15:51:24 +0800

    feat: 反馈增加图片

--- pages/tabbar/me/tools/advise.vue
+++ pages/tabbar/me/tools/advise.vue
@@ -15,9 +15,35 @@
 				placeholder="输入内容帮我们了解您的意见或建议" />
 			<span class="wordwrap">{{number}}/100</span>
 		</view>
+		
+		<!-- 图片依据 -->
+		<view class="uni-list list-pd">
+		    <view class="uni-list-cell cell-pd">
+		        <view class="uni-uploader">
+		            <view class="uni-uploader-body">
+		                <view class="uni-uploader__files">
+		                    <block v-for="(image, index) in imageList" :key="index">
+		                        <view class="uni-uploader__file" style="position: relative;">
+		                            <image class="uni-uploader__img" mode="aspectFit" :src="image" :data-src="image" @tap="previewImage"></image>
+		                            <view class="close-view" @click="close(index)">×</view>
+		                        </view>
+		                    </block>
+		                    <view class="uni-uploader__input-box" v-if="uploadType === 'image' ? imageList.length < 9 : imageList <= 1">
+		                        <view class="uni-uploader__input" @tap="chooseImage"></view>
+		                    </view>
+		                </view>
+		            </view>
+		            <view class="uni-uploader-head">
+		                <view class="uni-uploader-title"></view>
+		                <view class="uni-uploader-info">{{ imageList.length }}/9</view>
+		            </view>
+		        </view>
+		    </view>
+		</view>
+
 		<view class="bottom">
 			<view class="concact">
-				<text class="way">请留下真实联系方式 (邮箱/QQ/微信/手机号)</text>
+				<text class="way">请留下真实联系方式 (微信/手机号)</text>
 			</view>
 			<input name="contact" v-model="contact" class="concactContent" placeholder="请留下任一联系方式" bindinput="userNameInput" />
 		</view>
@@ -45,7 +71,18 @@
 </template>
 
 <script>
-	import {htmlEncode,showToastTit} from '@/utils/utils.js'
+	import {attachUpload,htmlEncode,showToastTit,compressImg} from '@/utils/utils.js';
+	import image from '@/store/image.js';
+	var sourceType = [
+	    ['camera'],
+	    ['album'],
+	    ['camera', 'album']
+	]
+	var sizeType = [
+	    ['compressed'],
+	    ['original'],
+	    ['compressed', 'original']
+	]
 	export default {
 		data() {
 			return {
@@ -60,7 +97,16 @@
 					{"word": "界面审美"},
 					{"word": "功能建议"},
 					{"word": "其他反馈"}
-				]
+				],
+				imageList: [],//反馈图片列表
+				sourceTypeIndex: 2,
+				sourceType: ['拍照', '相册', '拍照或相册'],
+				sizeTypeIndex: 2,
+				sizeType: ['压缩', '原图', '压缩或原图'],
+				countIndex: 8,
+				count: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+				uploadType: 'image',
+				show: false,
 			}
 		},
 		methods: {
@@ -72,7 +118,7 @@
 				this.list[index].check = this.list[index].check == false ? true : false;
 			},
 			// 用户反馈内容提交
-			formSubmit(e) {
+			formSubmit: async function (e) {
 				let words = this.list.filter(item => item.check == true).map(n => {
 					let {word} = {...n};
 					return {word}.word;
@@ -89,11 +135,17 @@
 					showToastTit('请添加联系方式')
 					return
 				}
+				// 获取上传图片地址
+				let images = [];
+				if (this.imageList.length != 0) {
+					images = await attachUpload(this.imageList);
+				}
 				let data={
 					username: this.$store.state.userInfo.username,
 					type: words.toString(),
 					content: htmlEncode(this.content),
-					contact: htmlEncode(this.contact)
+					contact: htmlEncode(this.contact),
+					images: images
 				}
 				this.$H.post('/zf/v1/advise/advises',data,true).then(res=>{
 					if(res.status){
@@ -110,6 +162,55 @@
 			// // 监听输入内容
 			descInput(e) {
 				this.number = e.detail.cursor
+			},
+			// 切换类型
+			uploadTypeChange(val){
+			    this.imageList = []
+			},
+			// 查看放大图片
+			previewImage: function (e) {
+			    var current = e.target.dataset.src
+			    uni.previewImage({
+			        current: current,
+			        urls: this.imageList
+			    })
+			},
+			// 移除照片
+			close(e) {
+			    this.imageList.splice(e, 1);
+			},
+			// 选择图片
+			chooseImage: async function () {
+			    let that = this;
+			    uni.showModal({
+			        title: '温馨提示',
+			        content: '获取相机或相册权限才可以上传图片',
+					showCancel: false,
+					confirmText: '继续',
+			        success(res) {
+			            if (res.confirm) {
+			                uni.chooseImage({
+			                    sourceType: sourceType[that.sourceTypeIndex],
+			                    sizeType: sizeType[that.sizeTypeIndex],
+			                    count: that.imageList.length + that.count[that.countIndex] > 9 ? 9 - that.imageList.length : that.count[that.countIndex],
+			                    success: (res) => {
+			                        // #ifdef APP-PLUS
+			                        // 提交压缩,因为使用了H5+ Api,所以自定义压缩目前仅支持APP平台
+			                        res.tempFilePaths.forEach(item => {
+			                            compressImg(item).then(cp_images => {
+			                                that.imageList = that.imageList.concat(cp_images)
+			                            })
+			                        })
+			                        // #endif
+			                        // #ifndef APP-PLUS
+			                        // 非APP平台不支持自定义压缩,暂时没有处理,可通过uni-app上传组件的sizeType属性压缩
+			                        that.imageList = that.imageList.concat(res.tempFilePaths)
+			                        // #endif
+			                    }
+			                })
+			            }
+			        }
+			    });
 			}
 		}
 	}
@@ -152,7 +253,7 @@
 	}
 	/* 反馈内容 */
 	.middle {
-		height: 35vh;
+		height: 15vh;
 		width: 100%;
 		position: relative;
 	}
@@ -179,6 +280,7 @@
 	}
 	/* 联系方式 */
 	.bottom {
+		margin-top: 20rpx;
 		height: 11vh;
 		width: 100%;
 	}
@@ -224,5 +326,112 @@
 			margin-bottom: 11rpx;
 			line-height: 150%;
 		}
+	}
+	.uni-list::before {
+	    height: 0;
+	}
+	.uni-list:after {
+	    height: 0;
+	}
+	.list-pd {
+	    margin-top: 0;
+		border-bottom: 1px solid #f2f2f2;
+	}
+	.cell-pd {
+	    padding: 12upx 30upx;
+	}
+	.uni-uploader-head {
+	    display: flex;
+	    flex-direction: row;
+	    justify-content: space-between;
+	}
+	.uni-uploader__files {
+	    display: flex;
+	    flex-direction: row;
+	    flex-wrap: wrap;
+	}
+	.uni-uploader__video{
+	    width: 100px;
+	    height: 100px;
+	}
+	.uni-uploader__input-box {
+	    position: relative;
+	    margin: 4px;
+	    width: 99px;
+	    height: 99px;
+	    border: 1px solid #D9D9D9;
+	}
+	.uni-uploader__input-box {
+	    position: relative;
+	    margin: 10upx;
+	    width: 208upx;
+	    height: 208upx;
+	    border: 1upx solid #D9D9D9;
+	}
+	.uni-uploader__input-box:before,
+	.uni-uploader__input-box:after {
+	    content: " ";
+	    position: absolute;
+	    top: 50%;
+	    left: 50%;
+	    -webkit-transform: translate(-50%, -50%);
+	    transform: translate(-50%, -50%);
+	    background-color: #D9D9D9;
+	}
+	.uni-uploader__input-box:before {
+	    width: 4upx;
+	    height: 79upx;
+	}
+	.uni-uploader__input-box:after {
+	    width: 79upx;
+	    height: 4upx;
+	}
+	.uni-uploader__input-box:active {
+	    border-color: #999999;
+	}
+	.uni-uploader__input-box:active:before,
+	.uni-uploader__input-box:active:after {
+	    background-color: #999999;
+	}
+	.uni-uploader__input {
+	    position: absolute;
+	    z-index: 1;
+	    top: 0;
+	    left: 0;
+	    width: 100%;
+	    height: 100%;
+	    opacity: 0;
+	}
+	.uni-uploader__file {
+	    margin: 4px 20upx;
+	    width: 100px;
+	    height: 100px;
+	    margin-left: 0;
+	}
+	.upload-type {
+	    height: 50px;
+	    display: flex;
+	    flex-flow: row nowrap;
+	    justify-content: space-between;
+	    align-items: center;
+	    margin: 0 30upx;
+	}
+	uni-image {
+	    width: 100px;
+	    height: 100px;
+	}
+	.close-view {
+	    text-align: center;
+	    line-height: 30upx;
+	    height: 35upx;
+	    width: 35upx;
+	    background: #ef5350;
+	    color: #FFFFFF;
+	    position: absolute;
+	    top: 1upx;
+	    right: 1upx;
+	    font-size: 35upx;
+	    border-radius: 8upx;
+	    z-index: 100;
 	}
 </style>
