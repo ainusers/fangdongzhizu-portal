888faa087a1a28de657e2bf923a07e76e82ad435
979111986@qq.com <979111986@qq.com>
2025-03-15 14:31:00 +0800

    feat: 社区增加loadmore/分享微信/温馨提示

--- components/post-list/post-list.vue
+++ components/post-list/post-list.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="main-style">
 		<block v-for="(item, index) in list" :key="index">
-			<view @click="toDetail(item,index)">
+			<view @click="toDetail(item,index,'')">
 				<view class="post-item">
 					<!-- 用户数据 -->
 					<view class="post-item-top-user">
@@ -11,30 +11,26 @@
 						</view>
 						<!-- 用户名称 -->
 						<view class="center">
-							<view style="display:flex;align-items: center;justify-content: space-between;">
-								<text class="nickname">{{ item.nickname?item.nickname.substring(0, 12):'' }}</text>
-								
-								<view style="float: right;padding-right: 10px;font-size: 18px;" @click.stop.prevent="goReport(index,$event)"
-									  v-if="item.userid==$store.state.userInfo.id">
-									<u-icon name="more-dot-fill" color="rgb(203,203,203)" ></u-icon>
-									<view class="reportText" v-if="item.isReport">
-										<view @click="deletePost(index,item.id)" class="item" >删除</view>
+							<view style="display:flex;">
+								<text class="nickname" style="flex:1">{{ item.nickname?item.nickname.substring(0, 12):'' }}</text>
+								<view style="flex:1"></view>
+								<template style="flex:1">
+									<text style="font-size: 14px;color:#5199ff;padding-right: 20rpx;">{{ item.score }}</text>
+									<view>
+										<view style="font-size: 18px;" @click.stop.prevent="goReport(index,$event)"
+											  v-if="item.userid==$store.state.userInfo.id">
+											<u-icon name="more-dot-fill" color="rgb(203,203,203)"style="display: flex;right:50rpx;" ></u-icon>
+											<view class="reportText" v-if="item.isReport">
+												<view @click="deletePost(index,item.id)" class="item" >删除</view>
+											</view>
+										</view>
 									</view>
-								</view>
-								<!-- <view style="float: right;padding-right: 10px;font-size: 18px;" @click.stop.prevent="goReport(index,$event)">
-									<u-icon name="more-dot-fill" color="rgb(203,203,203)" ></u-icon>
-									<view class="reportText" v-if="item.isReport">
-										<view @click="report" class="item"
-											v-if="item.userid!=$store.state.userInfo.id">举报</view>
-										<view @click="deletePost(index,item.id)" class="item"
-											v-if="item.userid==$store.state.userInfo.id">删除</view>
-									</view>
-								</view> -->
+								</template>
 							</view>
 							<view>
 								<text class="time">{{tranfTime(item.createtime)}}</text>
 								<block v-if="item.province">
-									<text class="city">发布于{{item.province | splitEnd1}}</text>
+									<text class="city">{{item.province}}</text>
 								</block>
 							</view>
 						</view>
@@ -84,27 +80,44 @@
 					</view>
 					<!-- 列表操作（浏览，评论，点赞） -->
 					<view class="p-footer">
+						<!-- 分享 -->
+						<view class="p-item">
+							<view v-if="!isDetail" @click.stop="toDetail(item,index,'zhuanfa')">
+								<u-icon name="zhuanfa"></u-icon>
+								<text class="count">{{ item.forward > 0 ? item.forward : '' }}</text>
+							</view>
+							<view v-else @click.stop="showShares">
+								<u-icon name="zhuanfa"></u-icon>
+								<text class="count">{{ item.forward > 0 ? item.forward : '' }}</text>
+							</view>
+						</view>						
 						<!-- 浏览 -->
 						<view class="p-item">
-							<button class="u-reset-button" open-type="share">
+							<view>
 								<u-icon name="eye" size="38"></u-icon>
-								<text class="count">{{ item.look }}</text>
-							</button>
+								<text class="count">{{ item.look > 0 ? item.look : '' }}</text>
+							</view>
 						</view>
 						<!-- 评论 -->
 						<view class="p-item margin50">
 							<u-icon name="chat" size="38"></u-icon>
-							<text class="count">{{ item.comment }}</text>
+							<text class="count">{{ item.comment > 0 ? item.comment : '' }}</text>
 						</view>
 						<!-- 点赞和取消点赞 -->
 						<view v-if="item.status==1" class="p-item"
 							@click.stop="cancelCollection(item.id, index, item.userid)">
 							<u-icon name="heart-fill" color="#cc0000" size="38"></u-icon>
-							<text class="count">{{ item.like>0 ?item.like :'' }}</text>
+							<text class="count">{{ item.like > 0 ? item.like : '' }}</text>
 						</view>
 						<view v-else class="p-item" @click.stop="addCollection(item.id, index, item.userid)">
 							<u-icon name="heart" size="38"></u-icon>
-							<text class="count">{{ item.like>0?item.like:'' }}</text>
+							<text class="count">{{ item.like>0 ? item.like : '' }}</text>
+						</view>
+					</view>
+					<!-- 温馨提示 -->
+					<view v-if="isDetail">
+						<view style="color:#AAAAAA; font-size: 14px;padding: 5px;">
+							基于言论自由的考量，暂未对以上内容进行审核，无法确保内容的真实和准确性，对此不做任何保证与承诺，强烈建议交易前谨慎核实，确保个人财产和信息安全，如遇问题，请立即联系平台
 						</view>
 					</view>
 				</view>
@@ -117,23 +130,37 @@
 			</view>
 		</block>
 		<!-- 判断是否加载数据 -->
-		<block v-if="list.length === 0 && loadStatus == 'nomore'">
-			<!-- margin-top="100" -->
-			<u-empty text="我可是有底线的" mode="favor"></u-empty>
-		</block>
-
-		<block v-if="loadStatus == 'loadmore'&&!isDetail">
-			<view style="padding: 30rpx 0;">
-				<u-loadmore :status="loadStatus" :load-text="loadText" />
-			</view>
-		</block>
-		<block v-else>
-			<!-- 此处空白，用于朋友圈详情页 -->
-		</block>
+		<view  v-if="!isDetail && list.length == 0">
+				<u-empty text="暂无数据" mode="favor"></u-empty>
+		</view>
+		<u-loadmore  v-else-if="!isDetail && list.length>0" :status="loadStatus" :load-text="loadText" />
 		<!-- 举报模态框 -->
 		<view v-if="reportShows">
 			<zhizuReport @cancelReport="cancelReport" @goReport="goReportText" :typeStr="reportType" :reportId="reportId" ref="report" />
 		</view>
+		<!-- 分享操作 appShare('1','qq','')-->
+		<u-popup  v-model="showShare" mode="bottom" :mask="true" border-radius="15" @close="closeShare">
+			<view class="share-wrap">
+				<view class="row">
+					<view @click.stop="appShare('weixin','WXSceneSession','0')" class="share-item">
+						<uni-icons custom-prefix="iconfont" type="icon-weixinhaoyou"  size="40"></uni-icons>
+						<text>微信好友</text>
+					</view>
+					<view @click.stop="appShare('weixin','WXSceneFavorite','0')" class="share-item">
+						<uni-icons custom-prefix="iconfont" type="icon-wechatEnshrine" size="40"></uni-icons>
+						<text>微信收藏</text>
+					</view>
+					<view @click.stop="appShare('weixin','WXSceneTimeline','0')" class="share-item">
+						<uni-icons custom-prefix="iconfont" type="icon-shejiaotubiao-02" size="40"></uni-icons>
+						<text>朋友圈</text>
+					</view>
+					<view @click.stop="copyCurrentUrl" class="share-item">
+						<uni-icons custom-prefix="iconfont" type="icon-fuzhilianjie" size="40"></uni-icons>
+						<text>复制链接</text>
+					</view>
+				</view>
+			</view>
+		</u-popup>
 	</view>
 </template>
 
@@ -141,7 +168,7 @@
 	let that = null;
 	import zhizuReport from '@/components/common/modal/report.vue'
 	import postListSkeleton from '@/components/post-list/post-list-skeleton.vue'
-	import {tranfTime} from '@/utils/utils.js'
+	import {tranfTime,getCurrentUrl,appShare} from '@/utils/utils.js'
 	
 	export default {
 		name: 'post-list',
@@ -160,7 +187,6 @@
 		data() {
 			return {
 				reportShows: false,
-				showShare: false,
 				loadText: {
 					loadmore: '轻轻上拉加载更多...',
 					loading: '努力加载中...',
@@ -173,17 +199,10 @@
 				currentIndex: '',
 				follow: '',//当前是否点赞状态
 				shareId:'',
-				jiaL:[1,2,3]
+				jiaL:[1,2,3],
+				showShare:false, //是否展示分享弹窗
+				shareUrl:'' //分享的地址
 			};
-		},
-		filters:{
-			splitEnd1(val){
-				if(val){
-					return val.substring(0,val.length-1)
-				}else{
-					return val
-				}
-			}
 		},
 		computed: {
 			timestamp() {
@@ -227,8 +246,6 @@
 			goReport(index) {
 				this.currentIndex = index
 				this.list[index].isReport ? this.isReport = false : this.isReport = true
-				console.log(this.list[index].isReport)
-				console.log(this.isReport);
 				this.reportId = this.list[index].id
 				this.$emit('changeStatus', index, this.isReport)
 			},
@@ -245,27 +262,32 @@
 				this.reportShows=false
 			},
 			// 跳转详情页
-			toDetail(data,index) {
-				// 判断用户是否登录
-				if(!this.$store.state.token){
-					uni.navigateTo({
-						url: '/pages/auth/login',
-					})
-					return;
-				}
-				this.isReport = false
-				this.$emit('changeStatus', index, false)
-				if (this.isDetail) {
-					if (this.$parent.$parent.comment_id) {
-						this.$emit('commontInt')
-					}
-					return;
-				}
-				this.$store.commit('communityInfo', data)
+			toDetail(data,index,type) {
 				this.dyId = data.id
-				uni.navigateTo({
-					url: "/pages/tabbar/community/comment?data=" + JSON.stringify(data)
-				})
+				if(type == 'zhuanfa'){
+					uni.navigateTo({
+						url: "/pages/tabbar/community/comment?id=" + this.dyId + "&userId=" + ''
+					})
+				}else{
+					// 判断用户是否登录
+					if(!this.$store.state.token){
+						uni.navigateTo({
+							url: '/pages/auth/login',
+						})
+						return;
+					}
+					this.isReport = false
+					this.$emit('changeStatus', index, false)
+					if (this.isDetail) {
+						if (this.$parent.comment_id) {
+							this.$emit('commontInt')
+						}
+						return;
+					}
+					uni.navigateTo({
+						url: "/pages/tabbar/community/comment?id=" + this.dyId+"&userId="+this.$store.state.userInfo.id
+					})
+				}
 			},
 			toUcenter(userId) {
 				if (this.isPersonal) return
@@ -304,7 +326,55 @@
 					type:'plus'
 				}
 				this.$H.patch('/zf/v1/dynamic/transfer',data,true).then(res=>{})
-			}
+			},
+			// 展示分享弹框
+			showShares() {
+				this.showShare = true;
+			},
+			// 关闭分享弹框
+			closeShare(){
+				this.showShare = false;
+			},
+			// uni-share分享
+			appShare(provider,scene,type) {
+				this.getCurrentUrl();
+				appShare(provider,scene,type,this.shareUrl,this.list[0].words,this.list[0].words);
+				this.forwardCount();
+				this.closeShare();
+			},
+			// 复制链接
+			copyCurrentUrl() {
+				this.getCurrentUrl();
+				uni.setClipboardData({
+				    data: this.shareUrl,
+				    success: function () {
+				        console.log('复制成功');
+				    },
+				    fail: function (err) {
+				        console.error('复制失败', err);
+				    }
+				});
+				this.forwardCount();
+				this.closeShare();
+			},
+			// 获取当前页面地址
+			getCurrentUrl(){
+				this.shareUrl = getCurrentUrl();
+			},
+			// 转发统计
+			forwardCount(){
+				// 登录后才会触发转发计数
+				if(this.$store.state.userInfo.id){
+					let data={
+						id:this.list[0].id,
+						userId:this.$store.state.userInfo.id,
+						type:'plus',
+						dynamicUserId:this.list[0].userid
+					}
+					this.$H.patch('/zf/v1/dynamic/forward ',data,true).then(res=>{
+					})
+				}
+			},
 		}
 	};
 </script>
@@ -322,6 +392,11 @@
 		padding: 5px 5px 0 5px;
 		.post-content {
 			margin-top: 10rpx;
+			.videoclass{
+				width: 100%;
+				object-fit: cover;
+				position: relative;
+			}
 			.img-style-1 {
 				display: block;
 				width: 100%;
@@ -434,24 +509,24 @@
 	}
 	// 分享弹窗
 	.share-wrap {
-		display: flex;
-		justify-content: space-between;
-		width: 80%;
 		margin: 0 auto;
-		padding: 30rpx;
+		padding: 30rpx 10rpx;
+		z-index: 999;
+		.row{
+			display: flex;
+			margin: 10rpx 0;
+		}
 		.share-item {
+			margin: 0 auto;
 			display: flex;
 			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			margin: 0;
 			image {
 				width: 100rpx;
 				height: 100rpx;
 			}
 			text {
 				font-size: 24rpx;
-				margin-top: 20rpx;
+				margin-top: 8rpx;
 			}
 		}
 	}
