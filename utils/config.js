2365259489050a18ed23b6eb9db84b198d82a662
979111986@qq.com <979111986@qq.com>
2025-03-16 18:58:46 +0800

    feat: 服务地址调整为生产环境

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
-// const domain = "http://ainusers.asia:31001" //生产环境
-const domain = "http://ainusers.asia:31002" //测试环境
+const domain = "http://ainusers.asia:31001" //生产环境
+// const domain = "http://ainusers.asia:31002" //测试环境
 // const domain = "http://localhost:31001" //本地调试
 
 const shareDomain = "http://ainusers.asia:31080"	// 分享域名
