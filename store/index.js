3f77e7498acde571c8493c7fa48bc9b123e01fe7
979111986@qq.com <979111986@qq.com>
2025-03-16 18:56:14 +0800

    feat: 更新版本号为1.0.13

--- store/index.js
+++ store/index.js
@@ -8,7 +8,7 @@ const store = new Vuex.Store({
 		token:'',
 		address:{},
 		ispublishSub:false, //是否点击到过开启定位服务界面
-		version:'1.0.12'
+		version:'1.0.13'
 	},
 	mutations: {
 		userInfo(state,obj){
--- uni_modules/uni-upgrade-center-app/utils/call-check-version.js
+++ uni_modules/uni-upgrade-center-app/utils/call-check-version.js
@@ -5,7 +5,7 @@ export default function() {
 			const data = {
 				action: 'checkVersion',
 				appid: '__UNI__190F3AB', //plus.runtime.appid,
-				appVersion:'1.0.12',
+				appVersion:'1.0.13',
 				wgtVersion: widgetInfo.version
 			}
 			uniCloud.callFunction({
