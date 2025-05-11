8b12f5550cfa5094650adeb127ae4b91582eb8a4
979111986@qq.com <979111986@qq.com>
2025-05-11 21:22:20 +0800

    feat: 修复风险企业列表已知问题

--- pages/tabbar/me/list/list.vue
+++ pages/tabbar/me/list/list.vue
@@ -4,8 +4,8 @@
 			<!-- 企业雇主指数及排名模块 -->
 			<view class="company-title">
 				<view class="search-container">
-					<input class="search-input" type="text" v-model="searchCompanyName" placeholder="请输入公司名称" />
-					<button style="color: #0076f6;border-color: #0076f6;" @click="search" class="search-button"
+					<input class="search-input" type="text" v-model="searchCompanyName" placeholder="请输入公司名称" @input="handleInput" />
+					<button style="color: #1183fb;border-color: #1183fb;" @click="search" class="search-button"
 						type="primary" plain="true" @tap="$u.throttle(search, 2000)">查询
 					</button>
 				</view>
@@ -14,50 +14,58 @@
 				<view v-if="index === 0">
 					<view class="top-item1" @click="toDetail(item.code)">
 						<view class="rank top1">{{ index + 1 }}</view>
-						<text class="name">{{ item.name }}</text>
-						<text class="score">
+						<view class="name">{{ item.name }}</view>
+						<view class="score">
 							<view>{{ item.score }}</view>
 							<view style="color: #252933;font-size: 12px;">风险指数</view>
-						</text>
+						</view>
 					</view>
 				</view>
 				<view v-else-if="index === 1">
 					<view class="top-item2" @click="toDetail(item.code)">
 						<view class="rank top2">{{ index + 1 }}</view>
-						<text class="name">{{ item.name }}</text>
-						<text class="score">
+						<view class="name">{{ item.name }}</view>
+						<view class="score">
 							<view>{{ item.score }}</view>
 							<view style="color: #252933;font-size: 12px;">风险指数</view>
-						</text>
+						</view>
 					</view>
 				</view>
 				<view v-else-if="index === 2">
 					<view class="top-item3" @click="toDetail(item.code)">
 						<view class="rank top3">{{ index + 1 }}</view>
-						<text class="name">{{ item.name }}</text>
-						<text class="score">
+						<view class="name">{{ item.name }}</view>
+						<view class="score">
 							<view>{{ item.score }}</view>
 							<view style="color: #252933;font-size: 12px;">风险指数</view>
-						</text>
+						</view>
 					</view>
 				</view>
 				<view v-else>
 					<view class="other-item" @click="toDetail(item.code)">
 						<view class="rank">{{ index + 1 }}</view>
-						<text class="name">{{ item.name }}</text>
-						<text class="score">
+						<view class="name">{{ item.name }}</view>
+						<view class="score">
 							<view>{{ item.score }}</view>
 							<view style="color: #252933;font-size: 12px;">风险指数</view>
-						</text>
+						</view>
 					</view>
 				</view>
 			</view>
 		</view>
-		
+
 		<!-- 编辑按钮 (当待审核、已发布、已下架时显示)-->
 		<view class="operate" @click="publish">
-			<uni-icons custom-prefix="iconfont" type="icon-bianji" color="#0076f6" size="30"></uni-icons>
-			<view style="color:#0076f6;font-size: 14px;">发布</view>
+			<uni-icons custom-prefix="iconfont" type="icon-bianji" color="#1183fb" size="30"></uni-icons>
+			<view style="color:#1183fb;font-size: 14px;">发布</view>
+		</view>
+
+		<view>
+			<u-loadmore v-if="rankingList.length>0" :status="status" :loading-text="loadingText"
+				:loadmore-text="loadmoreText" :nomore-text="nomoreText" icon-color="#1183fb" color="#999" />
+			<view v-if="nodataFlag && isShow">
+				<u-empty text="暂无数据" mode="favor"></u-empty>
+			</view>
 		</view>
 	</view>
 </template>
@@ -67,42 +75,102 @@
 		data() {
 			return {
 				rankingList: [],
-				searchCompanyName: ''
+				searchCompanyName: '',
+				status: 'loadmore',
+				loadmoreText: '轻轻上拉加载更多...',
+				loadingText: '努力加载中...',
+				nomoreText: '没有更多了',
+				nodataFlag: false,
+				isShow: false,
+				listPage: 1,
+				pageSize: 15,
+				changeFlag:false,
+				ScrollFlag:false
 			};
+
 		},
-		onLoad() {
+		onShow() {
+			this.listPage = 1;
+			this.rankingList = [];
 			// 查询风险企业清单
-			this.companyList();
+			this.search();
+		},
+		// 下拉刷新
+		onPullDownRefresh() {
+			this.listPage = 1;
+			this.rankingList = [];
+			this.nodataFlag = false;
+			this.isShow = false;
+			this.search();
+			uni.stopPullDownRefresh();
+		},
+		// 上拉加载
+		onReachBottom() {
+			if (!this.nodataFlag && !this.isShow) {
+				this.listPage++;
+				this.ScrollFlag = true;
+				this.search();
+			} else {
+				uni.showToast({
+					icon: 'none',
+					title: "小主,别使劲,已经到底了"
+				})
+				uni.stopPullDownRefresh();
+			}
 		},
 		methods: {
+			//监听输入框变化
+			handleInput(e){
+				this.changeFlag = true;
+			},
 			// 查询指定名称的公司
 			search() {
-				this.$H.get('/zf/v1/feel/name',{name: this.searchCompanyName},true).then(res=>{
-					if(res.status){
-						this.rankingList = res.data
+				if(!this.ScrollFlag){
+					if (this.changeFlag ) {
+						this.listPage = 1
+						this.rankingList = []
+						this.status = 'loadmore'
+						this.nodataFlag = false
+						this.isShow = false
+						this.ScrollFlag = false
+					}
+				}
+				let data = {
+					"page": this.listPage,
+					"size": this.pageSize,
+					"name": this.searchCompanyName
+				}
+				this.$H.get('/zf/v1/feel/list', data, true).then(res => {
+					if (res.status) {
+						if (res.data.length > 0) {
+							this.rankingList = [...this.rankingList, ...res.data]
+							if (res.data.length == this.pageSize) {
+								this.status = 'loadmore'
+							} else {
+								this.status = 'nomore'
+								this.isShow = true;
+							}
+						} else {
+							this.status = 'nomore'
+							this.nodataFlag = true;
+							this.isShow = true;
+						}
 					}
 				})
+				this.ScrollFlag = false
 			},
 			// 跳转反馈详情页
 			toDetail(code) {
 				uni.navigateTo({
-					url: '/pages/tabbar/me/list/list-detail?code='+code
+					url: '/pages/tabbar/me/list/list-detail?code=' + code
 				})
 			},
 			// 发布
-			publish(){
+			publish() {
 				uni.navigateTo({
 					url: '/pages/tabbar/me/list/feel'
 				})
 			},
-			// 查询风险企业清单
-			companyList(){
-				this.$H.get('/zf/v1/feel/list',{},true).then(res=>{
-					if(res.status){
-						this.rankingList = res.data
-					}
-				})
-			}
 		}
 	};
 </script>
